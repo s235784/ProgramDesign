0a72e684a9ff968ef9b8d09fcff80ab80b7e8933
Gary <1605295430@qq.com>
2022-10-13 14:40:12 +0800

    增加了管理员查看用户评价界面

---
+++ qt03/admin_checkuser.h
@@ -0,0 +1,26 @@
+#ifndef ADMIN_CHECKUSER_H
+#define ADMIN_CHECKUSER_H
+
+#include <QMainWindow>
+#include "admin_main.h"
+
+namespace Ui {
+class admin_checkuser;
+}
+
+class admin_checkuser : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit admin_checkuser(QWidget *parent = nullptr);
+    ~admin_checkuser();
+
+private slots:
+    void on_pushButton_back_clicked();
+
+private:
+    Ui::admin_checkuser *ui;
+};
+
+#endif // ADMIN_CHECKUSER_H
